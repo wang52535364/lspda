aeb19490a8b1f6b9ad0b69b60bb44c8e445edd64
yangruihuawinds <2538746471@qq.com>
2019-12-19 17:25:30 +0800

    5.10 职业认证  优化Java，实现button控件提交功能； 杨瑞华 刘泽钜

--- app/src/main/java/com/wulianwang/lsp/activity/ProfessionalCertificationActivity.java
+++ app/src/main/java/com/wulianwang/lsp/activity/ProfessionalCertificationActivity.java
@@ -3,8 +3,12 @@ package com.wulianwang.lsp.activity;
         import androidx.appcompat.app.AppCompatActivity;
 
         import android.os.Bundle;
+        import android.widget.Button;
+        import android.widget.ImageView;
         import android.widget.Spinner;
         import android.widget.ArrayAdapter;
+        import android.content.Intent;
+        import android.view.View;
 
         import com.wulianwang.lsp.R;
 
@@ -17,12 +21,28 @@ public class ProfessionalCertificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professional_certification);
-        Spinner sp;
-
-        sp = (Spinner) this.findViewById(R.id.spinner);
+        Spinner spinner;
+        Button btn;
+        ImageView imv;
+        Spinner sp = (Spinner)this.findViewById(R.id.spinner);
+        imv=(ImageView)this.findViewById(R.id.imageView4);
+        btn=(Button)this.findViewById(R.id.button2);
         String[] str = {"请选择职业类型", "水电工", "水工", "外卖员", "建筑工"};
         int String;
         ArrayAdapter<String> ad = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, str);
         sp.setAdapter(ad);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfessionalCertificationActivity.this, MyInfoActivity.class);
+                startActivity(intent);}
+        });
+        imv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(ProfessionalCertificationActivity.this, MyInfoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
