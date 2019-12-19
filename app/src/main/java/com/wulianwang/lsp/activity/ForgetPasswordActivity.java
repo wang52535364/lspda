b40287e1893f3700451fc6b44acedcc11d61f8f5
dk-hub <1135470697@qq.com>
2019-12-19 16:03:55 +0800

    张栋凯&曹小康 1-3 忘记密码 优化：1.优化了两次密码判断是否一致；      2.优化了三项输入内容不能为空；      3.优化了返回button按钮跳转到上个界面的功能；      4.优化了“确认修改”button按钮跳转到登陆界面；

--- app/src/main/java/com/wulianwang/lsp/activity/ForgetPasswordActivity.java
+++ app/src/main/java/com/wulianwang/lsp/activity/ForgetPasswordActivity.java
@@ -1,5 +1,6 @@
 package com.wulianwang.lsp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,32 +18,42 @@ import com.wulianwang.lsp.R;
 public class ForgetPasswordActivity extends AppCompatActivity {
 
     private ImageButton mybtn1;private Button mybtn2;
-    private  EditText editText1;private  EditText editText2;
-    String password;String password2;
+    private  EditText editText1;private  EditText editText2; private EditText editText;
+    String psaaword0; String password1; String password2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
-        editText1 = (EditText) findViewById(R.id.et_password);
+        editText = (EditText) findViewById(R.id.et_password0);
+        editText1 = (EditText) findViewById(R.id.et_password1);
         editText2 = (EditText) findViewById(R.id.et_password2);
         mybtn1 = (ImageButton) findViewById(R.id.bt1);
         mybtn2 = (Button) findViewById(R.id.bt2);
-        mybtn2.setOnClickListener(new View.OnClickListener() {
+        mybtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {finish();
+
+            }
+        });
+        mybtn2.setOnClickListener(new View.OnClickListener() { //给确认更改按钮设置监听
             @Override
             public void onClick(View v) {
-                password=editText1.getText().toString().trim();
-                password2=editText2.getText().toString().trim();
+                psaaword0=editText.getText().toString().trim();  //输入的手机号码
+                password1=editText1.getText().toString().trim();  //第一次输入的密码
+                password2=editText2.getText().toString().trim();  //第二次输入的密码
 
-               if(password.equals("")||password2.equals("")){
-                   Toast.makeText(getApplicationContext(),"密码不能为空",Toast.LENGTH_SHORT).show();
-               }else if (password.equals(password2)){
+               if(password1.equals("")||password2.equals("")||psaaword0.equals("")){//判断两次密码是否为空
+                   Toast.makeText(getApplicationContext(),"输入手机号码和密码不能为空",Toast.LENGTH_SHORT).show();
+               }else if (!password1.equals(password2)){
+                   Toast.makeText(getApplicationContext(),"密码输入不一致，请重新输入",Toast.LENGTH_SHORT).show();
+               }else if (password1.equals(password2)){
                    Toast.makeText(getApplicationContext(),"修改成功",Toast.LENGTH_SHORT).show();
-
-               }else if (password.equals("")  != password2.equals("")){
-                   Toast.makeText(getApplicationContext(),"密码不一致，请重新输入",Toast.LENGTH_SHORT).show();
-                }
-
-               finish();
+                    //把editext里面的密码上传到数据库
+                   Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                   startActivity(intent);
+                   finish();
+               }
             }
         });
 
